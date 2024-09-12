531c6bf97cef5346213030e6a8428de4add6444a
Samuel Spink <dr.greenjet@gmail.com>
2024-09-12 23:17:57 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        JWT_SECRET: process.env.JWT_SECRET,
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+      },
+};
 
 export default nextConfig;
